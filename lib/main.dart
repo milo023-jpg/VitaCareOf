031a26dc4b47bda8d98bc1c1ba8b1bb58ca81762
Camilo <miloar023@gmail.com>
2025-09-21 19:29:42 -0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:vitacareof/presentation/screens/login/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: LoginScreen()
-    );
+    return MaterialApp(title: 'Material App', home: LoginScreen());
   }
 }
--- lib/presentation/screens/login/login_screen.dart
+++ lib/presentation/screens/login/login_screen.dart
@@ -5,9 +5,51 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final focusNode = FocusNode();
+    final focusNode1 = FocusNode();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
+      appBar: AppBar(title: Text('Login')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           // Image(image: AssetImage('assets/images/vitacarelogo.png'),),          
+            // correo
+            SizedBox(
+              width: 300,
+              child: TextField(
+                focusNode: focusNode,
+                onTapOutside: (event) => focusNode.unfocus(),
+                decoration: InputDecoration(
+                  labelText: 'Correo',
+                  hintText: 'correo@example.com',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // contraseña
+            SizedBox(
+              width: 300,
+              child: TextField(
+                focusNode: focusNode1,
+                onTapOutside: (event) => focusNode1.unfocus(),
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  hintText: 'Contraseña',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            // contraseña
+            SizedBox(height: 10),
+            FilledButton(onPressed: () {}, child: Text('Inciar')),
+          ],
+        ),
       ),
     );
   }
